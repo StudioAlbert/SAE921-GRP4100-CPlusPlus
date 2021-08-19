9916cbfb0b1cc6a7040ec44d4445e5a3ad29ae76
Sébastien Albert <account@studio-albert.com>
2021-08-19 10:45:53 +0200

    add first stuff (Cmake, one empty project

---
+++ 00/src/main.cpp
@@ -0,0 +1,11 @@
+
+
+// ConsoleApplication3.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+}
